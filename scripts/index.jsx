d0c72ad122cbd2b0bb5a964e65b0c2fe2d36f25f
darshilv <dvora@dvora-ltm2.internal.salesforce.com>
2015-05-26 15:38:10 -0700

    Updated webpack and bootstrap configuration

--- scripts/index.jsx
+++ scripts/index.jsx
@@ -1,6 +1,8 @@
 'use strict';
 
 var React = require('react'),
-    App = require('./app');
+    App = require('./app/app');
+
+require('./scss/_boot.scss');
 
 React.render(<App />, document.body);
