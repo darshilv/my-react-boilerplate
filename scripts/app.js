72d87adf5f2a62d954ebf3e9eb4d984b7d0372c8
darshilv <dvora@dvora-ltm2.internal.salesforce.com>
2015-05-26 09:28:21 -0700

    Update package.json and app to include routes

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,10 @@
 'use strict';
 
 var React = require('react');
+var Router = require('react-router');
+var Route = Router.Route;
+var DefualtRoute = Router.DefualtRoute;
+var NotFoundRoute = Router.NotFoundRoute;
 
 var App = React.createClass({
   render() {
